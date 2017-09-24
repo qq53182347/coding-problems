189bf9324dcd186521b2e8431f7aa5b1dc925f83
qq53182347 <53182347@qq.com>
2017-09-24 16:21:56 +0800

    修改 删除数组中重复的元素

--- src/liugh/test_aritmetic/删除数组中重复的元素.java
+++ src/liugh/test_aritmetic/删除数组中重复的元素.java
@@ -3,22 +3,25 @@ package liugh.test_aritmetic;
 import java.util.Arrays;
 
 public class 删除数组中重复的元素 {
-	 public static int[] removeDuplicates(int a[]) {   
-	        if(a.length <= 1)  return a;   
-	        int index = 0;   
-	        for(int i = 1; i < a.length; i++) {   
-	            if(a[index] != a[i]) {   
-	                a[++index] = a[i];   
-	            }   
-	        }   
-	        int[] b = new int[index + 1];   
-	        System.arraycopy(a, 0, b, 0, b.length);   //a=[1, 2, 3, 2, 3]
-	        return b;   
-	   }   
-	       
-	   public static void main(String[] args) {   
-	        int[] a = {1, 1, 2, 2, 3};   
-	        a = removeDuplicates(a);   
-	        System.out.println(Arrays.toString(a));   
-	   }   
+	public static void main(String[] args) {   
+        int[] a = {1, 1, 2, 2, 3};   
+        a = removeDuplicates(a);   
+        System.out.println(Arrays.toString(a));   
+   }
+
+	private static int[] removeDuplicates(int[] a) {
+		if(a.length<=1){
+			return a;
+		}
+		int index=0;
+		for (int i = 0; i < a.length; i++) {
+			if(a[index]!=a[i]){
+				a[++index]=a[i];
+			}
+		}
+		System.out.println(Arrays.toString(a));   
+		int[] b = new int[index+1];
+		System.arraycopy(a, 0, b, 0, b.length);
+		return b;
+	}  
 }
